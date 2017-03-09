1e9dcd33d2a2ed32ab399c72d5ef4f455d97966a
Lorenzo <lferrario@byte-code.com>
2017-03-09 16:23:01 +0100

    add topHR and some global styles

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const HRtop = styled.div`
+  width: 100%;
+  height: 10px;
+  background-color: #333333;
+`;
 
 export default class App extends Component {
   props: {
@@ -8,6 +15,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <HRtop />
         {this.props.children}
       </div>
     );
