c9f61c726fa04884690ee6090a7003b07aad9c54
Lorenzo <lferrario@byte-code.com>
2017-03-13 18:09:16 +0100

    implement first draft of FamilyBadge

--- app/components/FamilyBadge.js
+++ app/components/FamilyBadge.js
@@ -4,21 +4,34 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   width: ${props => props.width}px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImageWrapper = styled.img`
   height: ${props => props.height}px;
-  background-color: gray;
-  position: relative;
+`;
+
+const NameWrapper = styled.div`
+  width: 100%;
+  background-color: #ffffff;
+  padding: 15px 10px;
+  color: #333333;
 `;
 
 const FamilyName = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #339900;
-  color: white;
-  min-height: 42px;
-  position: absolute;
-  bottom: 0;
+  height: 28px;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 1.4;
+  text-transform: uppercase;
+`;
+
+const ItemCount = styled.div`
+  height: 20px;
+  font-size: 16px;
+  line-height: 1.4;
+  font-style: italic;
 `;
 
 export default class FamilyBadge extends Component {
@@ -32,11 +45,15 @@ export default class FamilyBadge extends Component {
 
     const familyName = family.get('familyName');
     const image = family.get('image');
+    const itemCount = family.get('itemCount');
 
     return (
-      <Wrapper width={490} height={490}>
-        <img src={image} alt="alt" />
-        <FamilyName>{ familyName }</FamilyName>
+      <Wrapper width={490}>
+        <ImageWrapper src={image} alt="alt" height={490} />
+        <NameWrapper>
+          <FamilyName>{familyName}</FamilyName>
+          <ItemCount>{itemCount} prodotti</ItemCount>
+        </NameWrapper>
       </Wrapper>
     );
   }
