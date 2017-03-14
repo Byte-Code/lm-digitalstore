aa6a99274ac6c68e3529b5c719812e108745306a
Lorenzo <lferrario@byte-code.com>
2017-03-14 12:08:16 +0100

    add banner

--- app/components/Home.js
+++ app/components/Home.js
@@ -26,6 +26,16 @@ const DoubleVertical = styled.div`
   justify-content: space-between;
 `;
 
+const Banner = styled.div`
+  background: url(${props => props.url}) center repeat border-box padding-box;
+  height: 305px;
+  font-size: 32px;
+  padding: 30px 20px 47px;
+  color: #fff;
+  font-weight: bold;
+  font-style: italic;
+`;
+
 export default class Home extends Component {
   static propTypes = {
     fetchWorld: PropTypes.func.isRequired,
@@ -48,37 +58,37 @@ export default class Home extends Component {
     }
 
     const families = world.get('families');
+    const bannerText = world.getIn(['banner', 'text']);
+    const bannerImg = world.getIn(['banner', 'image']);
 
     return (
-      <div>
-        <FlexWrapper>
-          <Column>
-            <FamilyBadge family={families.get(0)} size="square-big" />
-            <FlexWrapper>
-              <DoubleVertical>
-                <FamilyBadge family={families.get(1)} size="square-small" />
-                <FamilyBadge family={families.get(2)} size="square-small" />
-              </DoubleVertical>
-              <FamilyBadge family={families.get(4)} size="vertical" />
-            </FlexWrapper>
-            <FamilyBadge family={families.get(7)} size="horizontal" />
-          </Column>
-          <Column>
-            <FlexWrapper>
-              <FamilyBadge family={families.get(6)} size="square-small" />
+      <FlexWrapper>
+        <Column>
+          <FamilyBadge family={families.get(0)} size="square-big" />
+          <FlexWrapper>
+            <DoubleVertical>
+              <FamilyBadge family={families.get(1)} size="square-small" />
               <FamilyBadge family={families.get(2)} size="square-small" />
-            </FlexWrapper>
-            <FamilyBadge family={families.get(3)} size="square-big" />
-            <FlexWrapper>
-              <DoubleVertical>
-                <FamilyBadge family={families.get(9)} size="square-small" />
-                <FamilyBadge family={families.get(5)} size="square-small" />
-              </DoubleVertical>
-              <FamilyBadge family={families.get(8)} size="vertical" />
-            </FlexWrapper>
-          </Column>
-        </FlexWrapper>
-      </div>
+            </DoubleVertical>
+            <FamilyBadge family={families.get(4)} size="vertical" />
+          </FlexWrapper>
+          <FamilyBadge family={families.get(7)} size="horizontal" />
+        </Column>
+        <Column>
+          <FlexWrapper>
+            <FamilyBadge family={families.get(6)} size="square-small" />
+            <Banner url={bannerImg}>{bannerText}</Banner>
+          </FlexWrapper>
+          <FamilyBadge family={families.get(3)} size="square-big" />
+          <FlexWrapper>
+            <DoubleVertical>
+              <FamilyBadge family={families.get(9)} size="square-small" />
+              <FamilyBadge family={families.get(5)} size="square-small" />
+            </DoubleVertical>
+            <FamilyBadge family={families.get(8)} size="vertical" />
+          </FlexWrapper>
+        </Column>
+      </FlexWrapper>
     );
   }
 }
