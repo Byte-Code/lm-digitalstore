cf4ebfa8e5cd845afd11d42e2ee0918fb4edfd10
Lorenzo <lferrario@byte-code.com>
2017-03-20 14:31:07 +0100

    test getWeatherSaga

--- test/sagas/sagas.spec.js
+++ test/sagas/sagas.spec.js
@@ -1,12 +1,21 @@
 import { call } from 'redux-saga/effects';
 
 import { callFetchWorld } from '../../app/sagas/getWorldSaga';
-import fetchWorld from '../../mocks/apiMock';
+import { callFetchWeather } from '../../app/sagas/getWeatherSaga';
+import { fetchWorld, fetchWeather } from '../../mocks/apiMock';
 
 describe('getWorldSaga ', () => {
   const gen = callFetchWorld();
 
   it('should call fetchWorld', () => {
     expect(gen.next().value).toEqual(call(fetchWorld));
+  });
+});
+
+describe('getWeatherSaga', () => {
+  const gen = callFetchWeather();
+
+  it('should call fetchWeather', () => {
+    expect(gen.next().value).toEqual(call(fetchWeather));
   });
 });
