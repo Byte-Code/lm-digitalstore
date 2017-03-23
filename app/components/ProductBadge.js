6a5d6409fd0e63eadeea4750428d902f4d34f176
Lorenzo <lferrario@byte-code.com>
2017-03-23 12:35:53 +0100

    display product name and title

--- app/components/ProductBadge.js
+++ app/components/ProductBadge.js
@@ -1,20 +1,47 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import styled from 'styled-components';
 
 import Image from './Image';
 
+const Wrapper = styled.div`
+  height: 605px;
+  width: 405px;
+  color: #333333;
+`;
+
+const Title = styled.div`
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  margin: 10px 0;
+`;
+
+const Name = styled.div`
+  line-height: 32px;
+  font-size: 20px;
+  text-align: center;
+  font-family: LeroyMerlinSans Bold;
+  margin: 10px 0;
+  text-transform: uppercase;
+`;
+
 const ProductBadge = ({ productInfo }) => {
   const imageID = productInfo.get('mainImage');
   const imageOptions = { width: 405, height: 405 };
   const name = productInfo.get('name');
+  const title = 'FACILE DA RIPORRE';
 
   return (
-    <Image
-      imageID={imageID}
-      imageOptions={imageOptions}
-      alt={name}
-    />
+    <Wrapper>
+      <Title>{title}</Title>
+      <Image
+        imageID={imageID}
+        imageOptions={imageOptions}
+        alt={name}
+      />
+      <Name>{name}</Name>
+    </Wrapper>
   );
 };
 
