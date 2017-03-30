7948bf89e515b86af0ba18f2be26205cbb405306
Lorenzo <lferrario@byte-code.com>
2017-03-30 16:57:46 +0200

    retrieve similar-products

--- app/sagas/getProductSaga.js
+++ app/sagas/getProductSaga.js
@@ -1,7 +1,7 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { fromJS } from 'immutable';
 
-import { apiV1 } from '../../mocks/apiMock';
+import { apiV1, apiMicro } from '../../mocks/apiMock';
 import * as actionTypes from '../actions/actionTypes';
 import * as productActions from '../actions/productActions';
 
@@ -9,8 +9,16 @@ import * as productActions from '../actions/productActions';
 export function* callFetchProduct(action) {
   try {
     const { productCode } = action;
-    const result = fromJS(yield call(apiV1.getProductDisplay.bind(apiV1), productCode))
-      .get('content');
+    const product = fromJS(
+      yield call(apiV1.getProductDisplay.bind(apiV1), productCode)
+    ).get('content');
+    const idList = fromJS(
+      yield call(apiMicro.getCrossSellingProducts.bind(apiMicro), productCode)
+    ).map(p => p.get('code'));
+    const similarProducts = fromJS(
+      yield call(apiV1.getProductListDisplay.bind(apiV1), idList.toJS())
+    ).getIn(['content', 'itemlist']);
+    const result = product.set('similarProducts', similarProducts);
     yield put(productActions.successFetchProduct(productCode, result));
   } catch (error) {
     yield put(productActions.failureFetchProduct(error));
