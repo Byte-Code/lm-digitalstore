2d4b2f06ebb835a6d6b06fe785687c60287f43eb
Lorenzo <lferrario@byte-code.com>
2017-04-04 10:17:56 +0200

    fix problem with getProductsToShow, now returning all products if no aids are selected

--- app/reducers/Catalogue/catalogueSelectors.js
+++ app/reducers/Catalogue/catalogueSelectors.js
@@ -2,6 +2,9 @@ import { List } from 'immutable';
 
 export function getProductsToShow(state, categoryCode, productIds) {
   if (state.has(categoryCode)) {
+    if (productIds.isEmpty()) {
+      return state.get(categoryCode);
+    }
     return state.get(categoryCode).filter(p => productIds.includes(p.get('code')));
   }
   return List();
