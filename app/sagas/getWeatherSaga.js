f039cf35f5e90eac005b371ba49ee7d16b156343
Lorenzo <lferrario@byte-code.com>
2017-04-11 16:46:11 +0200

    change api store code to 22

--- app/sagas/getWeatherSaga.js
+++ app/sagas/getWeatherSaga.js
@@ -3,12 +3,15 @@ import { fromJS } from 'immutable';
 
 import * as actionTypes from '../actions/actionTypes';
 import * as weatherActions from '../actions/weatherActions';
+import { requestFetchStore } from '../actions/storeActions';
 import { fetchWeather } from '../../mocks/apiMock';
 
 export function* callFetchWeather() {
   try {
     const result = fromJS(yield call(fetchWeather));
     yield put(weatherActions.successFetchWeather(result));
+    // TODO dispatch this action after storeCode calculations
+    yield put(requestFetchStore());
   } catch (error) {
     yield put(weatherActions.failureFetchWeather());
   }
--- mocks/apiMock.js
+++ mocks/apiMock.js
@@ -21,7 +21,7 @@ export function fetchWeather() {
 const baseUrl = 'https://api-gw-qa2.leroymerlin.it/api/v1';
 const spaceId = 'web-prd';
 const apiKey = 'testToken';
-const storeCode = 89;
+const storeCode = 22;
 export const apiV1 = new LmApi(baseUrl, spaceId, storeCode, apiKey);
 
 export const apiMicro = new LmApi(baseUrl, spaceId, storeCode, 'W7bWcKx6Vc');
