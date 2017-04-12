ec24fd5b8f5c2100b3a6997fd4c9c944eb15f2c9
jaga <kandros5591@gmail.com>
2017-04-12 12:33:30 +0200

    getStoreCodeFromIpAddress test

--- app/Pages/InitializationPage.js
+++ app/Pages/InitializationPage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import getIpAddresses from '../utils/get-ip-addresses';
 import {isWhitelisted, getStoreCodeFromIpAddress} from '../utils/store-code-utils';
-import {setstoreCode} from '../actions/storeCodeActions';
+import {setStoreCode} from '../actions/storeCodeActions';
 import {replace} from 'react-router-redux';
 import LinearProgress from 'material-ui/LinearProgress';
 import styled from 'styled-components';
@@ -52,12 +52,12 @@ class InitializationPage extends Component {
   
   componentDidUpdate() {
     const { ipAddress } = this.state;
-    const { replace, setstoreCode } = this.props;
+    const { replace, setStoreCode } = this.props;
     
     if (ipAddress) {
       if (isWhitelisted(ipAddress)) {
         const storeCode = getStoreCodeFromIpAddress(ipAddress)
-        setstoreCode(storeCode);
+        setStoreCode(storeCode);
       }
       else {
         replace('/store-selection');
@@ -83,4 +83,4 @@ class InitializationPage extends Component {
   }
 }
 
-export default connect(null, { replace, setstoreCode })(InitializationPage);
+export default connect(null, { replace, setStoreCode })(InitializationPage);
--- app/utils/store-code-utils.js
+++ app/utils/store-code-utils.js
@@ -2,10 +2,12 @@ const networkWhitelist = [36, 126, 127];
 
 export function isWhitelisted(ipAddress) {
   const bits = ipAddress.split('.');
-  return networkWhitelist.includes(bits[1])
+  return networkWhitelist.includes(
+    parseInt(bits[1])
+  );
 }
 
 export function getStoreCodeFromIpAddress(ipAddress) {
   const bits = ipAddress.split('.');
-  return Math.floor(bits[2] / 2)
+  return String(Math.floor(bits[2] / 2));
 }
