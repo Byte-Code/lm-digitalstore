117640658f3d29d69b335ff83d04fe9d6864d303
Lorenzo <lferrario@byte-code.com>
2017-04-12 17:36:43 +0200

    fix propTypes

--- app/reducers/Category/categorySelectors.js
+++ app/reducers/Category/categorySelectors.js
@@ -1,11 +1,11 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 export function getCategory(state, categoryCode) {
   return state.get(categoryCode);
 }
 
 export function getSellingAids(state, categoryCode) {
-  return state.getIn([categoryCode, 'sellingAidsProducts', 0, 'aids']) || List();
+  return state.getIn([categoryCode, 'sellingAidsProducts', 0, 'aids']) || Map();
 }
 
 export function getFilters(state, categoryCode) {
