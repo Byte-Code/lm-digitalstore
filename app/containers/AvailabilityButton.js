d8b5f8d4d82a7a072388208a505f1ac0a51befff
Lorenzo <lferrario@byte-code.com>
2017-04-13 10:42:22 +0200

    fix bug with nearbyStoresStock

--- app/containers/AvailabilityButton.js
+++ app/containers/AvailabilityButton.js
@@ -7,9 +7,9 @@ const mapStateToProps = (state, ownProps) => {
   const { nearbyStoreStock } = ownProps;
   const nearbyStores = getNearbyStores(state);
   const nearbyStocks = nearbyStores.map(s => {
-    const hasProduct = nearbyStoreStock.find(ns => ns.get('code') === s.get('code'));
+    const hasProduct = nearbyStoreStock.find(ns => ns.get('storeCode') === s.get('code'));
     if (hasProduct) {
-      return s.set('storeStock', (hasProduct.get('storeStock') - 2));
+      return s.set('storeStock', hasProduct.get('storeStock'));
     } return s.set('storeStock', 0);
   });
   return { nearbyStocks };
