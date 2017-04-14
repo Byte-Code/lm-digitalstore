f2761437b1f393bfcc9cbe94b141bf0f96af8638
jaga <kandros5591@gmail.com>
2017-04-14 13:23:16 +0200

    check interfaces inside getIpAddress instead of at startup

--- app/utils/get-ip-addresses.js
+++ app/utils/get-ip-addresses.js
@@ -1,7 +1,7 @@
 const os = require('os');
-const interfaces = os.networkInterfaces();
 
 export default function getIpAddresses() {
+  const interfaces = os.networkInterfaces();
   const addresses = [];
   for (let k in interfaces) {
     for (let k2 in interfaces[k]) {
