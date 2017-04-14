8e3fa782ffd11a73fb665587edd976c87f3ada5b
Lorenzo <lferrario@byte-code.com>
2017-04-14 13:41:05 +0200

    add button to close AvailabilityDialog

--- app/components/AvailabilityButton.js
+++ app/components/AvailabilityButton.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import Dialog from 'material-ui/Dialog';
 import styled from 'styled-components';
+import BlockIcon from 'material-ui/svg-icons/navigation/close';
 
 import AvailabilityDialog from './AvailabilityDialog';
 
@@ -18,6 +19,29 @@ const Button = styled.div`
   box-shadow:  0 0 8px 0 rgba(51, 51, 51, 0.1);
   cursor: pointer;
 `;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: flex-end;\
+  background: rgb(51, 51, 51);
+  padding: 10px;
+`;
+
+const TitleComponent = ({ handleClick }) => (
+  <TitleWrapper>
+    <BlockIcon
+      style={{ height: 50, width: 50, cursor: 'pointer' }}
+      color="#fff"
+      onTouchTap={handleClick}
+    />
+  </TitleWrapper>
+);
+
+TitleComponent.propTypes = {
+  handleClick: PropTypes.func.isRequired
+};
 
 export default class AvailabilityButton extends Component {
   static propTypes = {
@@ -60,7 +84,8 @@ export default class AvailabilityButton extends Component {
           onRequestClose={this.handleClose}
           open={this.state.dialogOpen}
           contentStyle={{ width: 1000, maxWidth: 'none' }}
-          bodyStyle={{ padding: '80px 75px', background: '#333333' }}
+          bodyStyle={{ padding: '20px 75px', background: '#333333' }}
+          title={<TitleComponent handleClick={this.handleClose} />}
         >
           <AvailabilityDialog
             nearbyStoreStock={nearbyStoreStock}
