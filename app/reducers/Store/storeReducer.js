f7c5eefa2ec3f4fdbb5b8406c1b1995ffc634b52
Lorenzo <lferrario@byte-code.com>
2017-04-19 12:23:06 +0200

    save nearbyStores in reducer

--- app/reducers/Store/storeReducer.js
+++ app/reducers/Store/storeReducer.js
@@ -4,6 +4,8 @@ export default function storeReducer(state = Map(), action) {
   switch (action.type) {
     case 'SUCCESS_FETCH_STORE':
       return action.result;
+    case 'SUCCESS_FETCH_NEARBYSTORES':
+      return state.set('nearbyStores', action.result);
     default:
       return state;
   }
