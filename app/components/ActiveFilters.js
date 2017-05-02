165898e60ea8c77b227daafb8dc1ff0a48d7bfde
Lorenzo <lferrario@byte-code.com>
2017-05-02 11:55:32 +0200

    move ActiveFilters to a different component

---
+++ app/components/ActiveFilters.js
@@ -0,0 +1,112 @@
+import React, { PropTypes } from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { List } from 'immutable';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background: ${props => props.bgColor};
+  padding: 0 20px;
+  width: 100%;
+  &>p {
+    font-family: LeroyMerlinSans Italic;
+    font-size: 16px;
+  }
+  &>div {
+    margin-right: 10px;
+  }
+`;
+
+const Filter = styled.div`
+  width: ${props => props.width || '150px'};
+  height: 42px;
+  border-radius: 20px;
+  background-color: ${props => (props.isActive ? '#67cb33' : '#efefef')};
+  color: ${props => (props.isActive ? '#efefef' : '#67cb33')}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  &>p {
+    line-height: 20px;
+    font-size: 16px;
+    text-align: center;
+  }
+`;
+
+const ResetButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  cursor: pointer;
+  &>p {
+    font-size: 16px;
+    text-decoration: underline;
+    color: #fff;
+  }
+`;
+
+const ActiveFilters = (props) => {
+  const {
+    filterMap,
+    filterGroups,
+    resetFilters,
+    toggleFilter,
+    toggleAvailability,
+    handleOpen
+  } = props;
+
+  const activeFilters = filterMap.get('activeFilters');
+  const activeAvailability = filterMap.get('activeAvailability');
+
+  if (activeFilters.isEmpty() && !activeAvailability) {
+    return (
+      <Wrapper bgColor="#e4e4e4">
+        <p>Tocca per avviare una ricerca avanzata dei prodotti</p>
+      </Wrapper>
+    );
+  }
+
+  const toTake = activeAvailability ? 2 : 3;
+  const activeFilterList = filterGroups.reduce((acc, val) =>
+    acc.push(val.get('filters').filter(f => activeFilters.includes(f.get('code'))))
+    , List()).filterNot(g => g.isEmpty()).flatten(true).take(toTake);
+
+  return (
+    <Wrapper bgColor="rgba(51, 51, 51, 0.8)">
+      {
+        activeFilterList.map(f => (
+          <Filter key={f.get('code')} isActive onClick={() => toggleFilter(f.get('code'))}>
+            <p>{f.get('name')}</p>
+          </Filter>))
+      }
+      { activeAvailability &&
+        (<Filter isActive onClick={toggleAvailability}>
+          <p>Disponibile</p>
+        </Filter>)
+      }
+      { activeFilters.size > toTake &&
+        (<Filter width="178px" onClick={handleOpen}>
+          <p>Visualizza altri filtri</p>
+        </Filter>)
+      }
+      <ResetButton onClick={resetFilters}>
+        <p>Reset filtri</p>
+      </ResetButton>
+    </Wrapper>
+  );
+};
+
+ActiveFilters.propTypes = {
+  filterGroups: ImmutablePropTypes.list.isRequired,
+  resetFilters: PropTypes.func.isRequired,
+  handleOpen: PropTypes.func.isRequired,
+  toggleFilter: PropTypes.func.isRequired,
+  filterMap: ImmutablePropTypes.map.isRequired,
+  toggleAvailability: PropTypes.func.isRequired
+};
+
+export default ActiveFilters;
--- app/components/FilterBar.js
+++ app/components/FilterBar.js
@@ -1,11 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import { List } from 'immutable';
 import styled from 'styled-components';
 import Dialog from 'material-ui/Dialog';
 import AddIcon from 'material-ui/svg-icons/content/add-circle-outline';
 
 import FilterDialog from './FilterDialog';
+import ActiveFilters from './ActiveFilters';
 
 const Wrapper = styled.div`
   display: flex;
@@ -24,52 +24,6 @@ const Button = styled.div`
     line-height: 32px;
     font-family: LeroyMerlinSans Light;
     margin-left: 12px;
-    color: #fff;
-  }
-`;
-
-const ActiveFilters = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  background: ${props => props.bgColor};
-  padding: 0 20px;
-  width: 100%;
-  &>p {
-    font-family: LeroyMerlinSans Italic;
-    font-size: 16px;
-  }
-  &>div {
-    margin-right: 10px;
-  }
-`;
-
-const Filter = styled.div`
-  width: ${props => props.width || '150px'};
-  height: 42px;
-  border-radius: 20px;
-  background-color: ${props => (props.isActive ? '#67cb33' : '#efefef')};
-  color: ${props => (props.isActive ? '#efefef' : '#67cb33')}
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  &>p {
-    line-height: 20px;
-    font-size: 16px;
-    text-align: center;
-  }
-`;
-
-const ResetButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: auto;
-  cursor: pointer;
-  &>p {
-    font-size: 16px;
-    text-decoration: underline;
     color: #fff;
   }
 `;
@@ -99,53 +53,15 @@ export default class FilterBar extends Component {
     this.setState({ open: false });
   }
 
-  // TODO separate this logic into another component?
-  renderActiveFilters() {
-    const { filterMap, filterGroups, resetFilters, toggleFilter, toggleAvailability } = this.props;
-    const activeFilters = filterMap.get('activeFilters');
-    const activeAvailability = filterMap.get('activeAvailability');
-    if (activeFilters.isEmpty() && !activeAvailability) {
-      return (
-        <ActiveFilters bgColor="#e4e4e4">
-          <p>Tocca per avviare una ricerca avanzata dei prodotti</p>
-        </ActiveFilters>
-      );
-    }
-    const toTake = activeAvailability ? 2 : 3;
-    const activeFilterList = filterGroups.reduce((acc, val) =>
-      acc.push(val.get('filters').filter(f => activeFilters.includes(f.get('code'))))
-      , List()).filterNot(g => g.isEmpty()).flatten(true).take(toTake);
-    return (
-      <ActiveFilters bgColor="rgba(51, 51, 51, 0.8)">
-        {
-          activeFilterList.map(f => (
-            <Filter key={f.get('code')} isActive onClick={() => toggleFilter(f.get('code'))}>
-              <p>{f.get('name')}</p>
-            </Filter>))
-        }
-        { activeAvailability &&
-          (<Filter isActive onClick={toggleAvailability}>
-            <p>Disponibile</p>
-          </Filter>)
-        }
-        { activeFilters.size > toTake &&
-          (<Filter width="178px" onClick={this.handleOpen}>
-            <p>Visualizza altri filtri</p>
-          </Filter>)
-        }
-        <ResetButton onClick={resetFilters}>
-          <p>Reset filtri</p>
-        </ResetButton>
-      </ActiveFilters>
-    );
-  }
-
   // TODO separate dialog logic into dialog component?
   render() {
     const {
       applyFilters,
       filterGroups,
-      filterMap
+      filterMap,
+      resetFilters,
+      toggleFilter,
+      toggleAvailability
     } = this.props;
 
     if (filterGroups.isEmpty()) {
@@ -158,7 +74,14 @@ export default class FilterBar extends Component {
           <AddIcon color="#fff" style={{ height: 30, width: 30 }} />
           <p>Piú filtri</p>
         </Button>
-        {this.renderActiveFilters()}
+        <ActiveFilters
+          filterMap={filterMap}
+          filterGroups={filterGroups}
+          resetFilters={resetFilters}
+          toggleFilter={toggleFilter}
+          toggleAvailability={toggleAvailability}
+          handleOpen={this.handleOpen}
+        />
         <Dialog
           modal={false}
           onRequestClose={this.handleClose}
