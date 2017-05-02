074eb24c70f7e6e4b590114a288ac459d104dc77
Lorenzo <lferrario@byte-code.com>
2017-05-02 16:41:32 +0200

    test footer and add matchMedia to setup.js

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,10 @@
 import { jsdom } from 'jsdom';
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
+
+window.matchMedia = window.matchMedia
+|| (() => ({ matches: false, addListener: () => {}, removeListener: () => {}, }));
+
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
 window.localStorage = window.sessionStorage = {
