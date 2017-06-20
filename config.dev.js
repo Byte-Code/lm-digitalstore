fc090048a99bf86b75335d679add4bc451f45818
Rinaldo <rrossi@byte-code.com>
2017-06-20 11:27:26 +0200

    add dev config

--- config.dev.js
+++ config.dev.js
@@ -1,21 +1,13 @@
 export default {
   apiConfig: {
-    baseUrlCore: 'https://api.leroymerlin.it/api/v1',
-    baseUrlMS: 'https://api.leroymerlin.it',
+    baseUrlCore: 'https://api-gw-qa.leroymerlin.it/api/v1',
+    baseUrlMS: 'https://api-gw-qa.leroymerlin.it',
     spaceId: 'web-prd',
-    storeCode: 7,
-    apiKeyCore: 'wYv7iVtxEU',
-    apiKeyMS: '96zoJArqvUf0'
+    storeCode: 89,
+    apiKeyCore: 'testToken',
+    apiKeyMS: 'W7bWcKx6Vc'
   },
   analytics: {
     debug: true
   }
 };
-
-/*
-baseUrlCore: 'https://api-gw-qa.leroymerlin.it/api/v1',
-  baseUrlMS: 'https://api-gw-qa.leroymerlin.it',
-  spaceId: 'web-prd',
-  storeCode: 89,
-  apiKeyCore: 'testToken',
-  apiKeyMS: 'W7bWcKx6Vc' */
