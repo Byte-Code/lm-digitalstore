bf19fb20a35728bfec73fb180ea0d89add85f4b7
Rinaldo <rrossi@byte-code.com>
2017-06-20 10:57:22 +0200

    add process config for different environments

---
+++ config.dev.js
@@ -0,0 +1,21 @@
+export default {
+  apiConfig: {
+    baseUrlCore: 'https://api.leroymerlin.it/api/v1',
+    baseUrlMS: 'https://api.leroymerlin.it',
+    spaceId: 'web-prd',
+    storeCode: 7,
+    apiKeyCore: 'wYv7iVtxEU',
+    apiKeyMS: '96zoJArqvUf0'
+  },
+  analytics: {
+    debug: true
+  }
+};
+
+/*
+baseUrlCore: 'https://api-gw-qa.leroymerlin.it/api/v1',
+  baseUrlMS: 'https://api-gw-qa.leroymerlin.it',
+  spaceId: 'web-prd',
+  storeCode: 89,
+  apiKeyCore: 'testToken',
+  apiKeyMS: 'W7bWcKx6Vc' */
---
+++ config.prod.js
@@ -0,0 +1,13 @@
+export default {
+  apiConfig: {
+    baseUrlCore: 'https://api.leroymerlin.it/api/v1',
+    baseUrlMS: 'https://api.leroymerlin.it',
+    spaceId: 'web-prd',
+    storeCode: 7,
+    apiKeyCore: 'wYv7iVtxEU',
+    apiKeyMS: '96zoJArqvUf0'
+  },
+  analytics: {
+    debug: false
+  }
+};
