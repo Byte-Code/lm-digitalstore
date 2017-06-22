3175a256b3cc064d4276429c877a2089f8f25b31
Rinaldo <rrossi@byte-code.com>
2017-06-22 14:28:42 +0200

    delete content in eventsMap for production build problem

--- app/analytics/eventsMap.js
+++ app/analytics/eventsMap.js
@@ -1,16 +1,13 @@
-import AnalyticsService from './AnalyticsService';
-import * as actions from '../actions/actionTypes';
+// import AnalyticsService from './AnalyticsService';
+// import * as actions from '../actions/actionTypes';
 
+/*
 const pageView = () => ({
   hitType: 'view',
   dataLayer: AnalyticsService.getDataLayer()
 });
+*/
 
-const eventsMap = {
-  '@@router/LOCATION_CHANGE': AnalyticsService.setPageName,
-  [actions.SET_SESSION_CODE]: AnalyticsService.setCid,
-  [actions.SET_STORE_CODE]: AnalyticsService.setStoreCode,
-  [actions.TRACK_SESSION_START]: pageView
-};
+const eventsMap = {};
 
 export default eventsMap;
