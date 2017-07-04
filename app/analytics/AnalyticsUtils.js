6c663b4e04ad5a108e6c40942c90bb1545767f05
Rinaldo <rrossi@byte-code.com>
2017-07-04 15:57:53 +0200

    fix code style

--- app/analytics/AnalyticsUtils.js
+++ app/analytics/AnalyticsUtils.js
@@ -17,91 +17,121 @@ const productPropertiesMap = Map({
 
 const layerMap = Map({ filter_name: List(), filter_value: List() });
 
-const buildCommonLayer = (product) => {
-  const commonPropertiesLayer = productPropertiesMap.reduce((acc, property, key) => {
-    const productProperty = product.hasIn(property) ? product.getIn(property) : 'null';
-    return acc.set(key, List().push(productProperty));
-  }, Map({}));
+const LABEL = {
+  PROD_SCONTO: 'prod_sconto',
+  PROD_AVAIL_ONLINE: 'prod_avail_online',
+  PROD_AVAIL_STORE: 'prod_avail_store',
+  PROD_VARIANT: 'prod_variant',
+  PROD_PUNTI_OMAGGIO: 'prod_puntiomaggio',
+  PROD_IDEAPIU: 'prod_idepiu',
+  PROD_BUNDLE: 'prod_bundle',
+  FILTER_NAME: 'filter_name',
+  FILTER_VALUE: 'filter_value',
+  FILTER_RESULT: 'filter_result',
+  PROD_POSITION: 'prod_position',
+  PROD_LIST: 'prod_list'
+};
 
-  const normalizedCommonPropertiesLayer = normalizeProperties(commonPropertiesLayer);
+const relatedProductsSize = 12;
+
+const buildCommonLayer = product => {
+  const commonPropertiesLayer = productPropertiesMap.reduce(
+    (acc, property, key) => {
+      const productProperty = product.hasIn(property)
+        ? product.getIn(property)
+        : 'null';
+      return acc.set(key, List().push(productProperty));
+    },
+    Map({})
+  );
+
+  const normalizedCommonPropertiesLayer = normalizeProperties(
+    commonPropertiesLayer
+  );
   return normalizedCommonPropertiesLayer;
 };
 
-const normalizeProperties = (layer) => {
-  let obj = layer;
-  obj = obj.set('prod_sconto', normalizeSconto(obj.get('prod_sconto')));
-  obj = obj.set('prod_avail_online', normalizeAvail(obj.get('prod_avail_online')));
-  obj = obj.set('prod_avail_store', normalizeAvail(obj.get('prod_avail_store')));
-  return obj;
-};
+const normalizeProperties = layer =>
+  layer
+    .set(LABEL.PROD_SCONTO, normalizeSconto(layer.get(LABEL.PROD_SCONTO)))
+    .set(
+      LABEL.PROD_AVAIL_ONLINE,
+      normalizeAvail(layer.get(LABEL.PROD_AVAIL_ONLINE))
+    )
+    .set(
+      LABEL.PROD_AVAIL_STORE,
+      normalizeAvail(layer.get(LABEL.PROD_AVAIL_STORE))
+    );
 
 const normalizeSconto = (value = Map({})) => {
   const sconto = value.get(0);
   return List().push(sconto !== 'null' ? Math.round(sconto * 10) : sconto);
 };
 
-const normalizeAvail = (field = List()) => List().push(field.get(0) ? '1' : '0');
+const normalizeAvail = (field = List()) =>
+  List().push(field.get(0) ? '1' : '0');
 
-const isProductNew = (product) => {
+const isProductNew = product => {
   const marketingAttributes = product.get('marketingAttributes');
   const loyaltyProgram = product.get('loyaltyProgram');
 
   if (marketingAttributes && loyaltyProgram) {
     const promotions = getPromotions(marketingAttributes, loyaltyProgram);
     const filteredPromotions = filterPromotions(promotions);
-    const isNew = filteredPromotions
-                  .reduce((acc, promotion) => promotion.get('code') === 'NOVITA', false);
+    const isNew = filteredPromotions.reduce(
+      (acc, promotion) => promotion.get('code') === 'NOVITA',
+      false
+    );
 
     const prodNew = isNew ? '1' : '0';
     return Map({ prod_new: List().push(prodNew) });
   }
 };
 
-const getVariant = (product) => {
+const getVariant = product => {
   const masterProductCode = product.get('masterProductCode');
-  const list = List();
-  let layer = Map({});
+  const value = masterProductCode
+    ? `${product.get('code')}_${masterProductCode}`
+    : 'master';
 
-  if (masterProductCode) {
-    const code = product.get('code');
-    layer = layer.set('prod_variant', list.push(`${code}_${masterProductCode}`));
-  } else {
-    layer = layer.set('prod_variant', list.push('master'));
-  }
-  return layer;
+  return Map({ [LABEL.PROD_VARIANT]: List().push(value) });
 };
 
-const getGiftPoints = (product) => {
+const getGiftPoints = product => {
   const giftPoints = product.getIn(['loyaltyProgram', 'type']);
   const list = List();
   let layer = Map({});
 
   if (giftPoints && giftPoints === 'ADDITIONAL_POINTS') {
-    const points = list.push(Math.round(product.getIn('loyaltyProgram', 'value') * 10));
-    layer = layer.set('prod_puntiomaggio', points);
+    const points = list.push(
+      Math.round(product.getIn('loyaltyProgram', 'value') * 10)
+    );
+    layer = layer.set(LABEL.PROD_PUNTI_OMAGGIO, points);
   }
   return layer;
 };
 
-const getIdeapiuPoints = (product) => {
+const getIdeapiuPoints = product => {
   const ideapiuPoints = product.getIn(['loyaltyProgram', 'type']);
   const list = List();
   let layer = Map({});
 
   if (ideapiuPoints && ideapiuPoints === 'DISCOUNT') {
-    const points = list.push(Math.round(product.getIn('prod_idepiu', 'value') * 10));
-    layer = layer.set('prod_idepiu', points);
+    const points = list.push(
+      Math.round(product.getIn(LABEL.PROD_IDEAPIU, 'value') * 10)
+    );
+    layer = layer.set(LABEL.PROD_IDEAPIU, points);
   }
   return layer;
 };
 
-const getBundle = (product) => {
+const getBundle = product => {
   const isBundle = product.getIn(['bundleInformation', 'isBundle']);
   const list = List();
   let layer = Map({ prod_bundle: list.push('0') });
 
   if (isBundle) {
-    layer = layer.set('prod_bundle', list.push('1'));
+    layer = layer.set(LABEL.PROD_BUNDLE, list.push('1'));
   }
   return layer;
 };
@@ -123,15 +153,19 @@ const buildAppliedFilters = (filterGroup, appliedFilters) => {
     const groupName = group.get('group');
     let accumulator = acc;
 
-    const activeFilters = group.get('filters').filter((filter) =>
-      appliedFilters.includes(filter.get('code'))
-    );
+    const activeFilters = group
+      .get('filters')
+      .filter(filter => appliedFilters.includes(filter.get('code')));
 
     if (activeFilters.size) {
       const newLayer = buildLayer(activeFilters, groupName);
       accumulator = accumulator
-        .updateIn(['filter_name'], arr => arr.concat(newLayer.get('filter_name')))
-        .updateIn(['filter_value'], arr => arr.concat(newLayer.get('filter_value')));
+        .updateIn([LABEL.FILTER_NAME], arr =>
+          arr.concat(newLayer.get(LABEL.FILTER_NAME))
+        )
+        .updateIn([LABEL.FILTER_VALUE], arr =>
+          arr.concat(newLayer.get(LABEL.FILTER_VALUE))
+        );
     }
 
     return accumulator;
@@ -141,35 +175,47 @@ const buildAppliedFilters = (filterGroup, appliedFilters) => {
 };
 
 const buildLayer = (activeFilters = List(), groupName = '') =>
-  activeFilters.reduce((acc, activeFilter) =>
+  activeFilters.reduce(
+    (acc, activeFilter) =>
       acc
-        .updateIn(['filter_name'], arr => arr.push(groupName))
-        .updateIn(['filter_value'], arr => arr.push(activeFilter.get('name'))),
+        .updateIn([LABEL.FILTER_NAME], arr => arr.push(groupName))
+        .updateIn([LABEL.FILTER_VALUE], arr =>
+          arr.push(activeFilter.get('name'))
+        ),
     layerMap
   );
 
 const buildSellingAid = (sellingAids, currentAid) => {
   const aidGroupName = sellingAids.get('userText');
-  const { name } = sellingAids.get('aids')
+  const { name } = sellingAids
+    .get('aids')
     .filter(aid => aid.get('code') === currentAid)
     .get(0)
     .toJS();
 
   return layerMap
-    .updateIn(['filter_name'], arr => arr.push(aidGroupName))
-    .updateIn(['filter_value'], arr => arr.push(name));
+    .updateIn([LABEL.FILTER_NAME], arr => arr.push(aidGroupName))
+    .updateIn([LABEL.FILTER_VALUE], arr => arr.push(name));
 };
 
 const buildFiltersDataLayer = (layerDataList = []) =>
-  layerDataList.reduce((acc, data) =>
+  layerDataList.reduce(
+    (acc, data) =>
       acc
-        .updateIn(['filter_name'], arr => arr.concat(data.get('filter_name')))
-        .updateIn(['filter_value'], arr => arr.concat(data.get('filter_value'))),
-    layerMap);
-
+        .updateIn([LABEL.FILTER_NAME], arr =>
+          arr.concat(data.get(LABEL.FILTER_NAME))
+        )
+        .updateIn([LABEL.FILTER_VALUE], arr =>
+          arr.concat(data.get(LABEL.FILTER_VALUE))
+        ),
+    layerMap
+  );
 
 const addResultToLayer = (productsNumber = 0, dataLayer = Map({})) =>
-  dataLayer.set('filter_result', productsNumber > 12 ? '12' : productsNumber.toString());
+  dataLayer.set(
+    LABEL.FILTER_RESULT,
+    productsNumber > 12 ? '12' : productsNumber.toString()
+  );
 
 // ---------------------   EXPORTED FUNCTIONS ------------------->
 
@@ -208,28 +254,38 @@ const buildProductLayer = (product = {}) => {
 };
 
 const buildRelatedProductsLayer = (products = List(), path = []) => {
-  const productList = products.size > 12 ? List(products).setSize(12) : products;
+  const productList = products.size > relatedProductsSize
+    ? List(products).setSize(relatedProductsSize)
+    : products;
+
   let prodPositionCount = 0;
 
   if (products.size) {
-    const listOfProductsLayer = productList.map((product) => {
-      const prodPosition = List().push(prodPositionCount += 1);
+    const listOfProductsLayer = productList.map(product => {
+      const prodPosition = List().push((prodPositionCount += 1));
       const prodListList = List().push(getProdList(product, path));
 
       let productLayer = buildCommonLayer(product);
       // add additional properties to productLayer
-      productLayer = productLayer.set('prod_position', prodPosition);
-      productLayer = productLayer.set('prod_list', prodListList);
+      productLayer = productLayer.set(LABEL.PROD_POSITION, prodPosition);
+      productLayer = productLayer.set(LABEL.PROD_LIST, prodListList);
       return productLayer;
     });
 
-    const mergedlistOfProductsLayer = listOfProductsLayer.reduce((acc, productLayer) => {
-      const mergedValues = _.mergeWith(acc.toJS(), productLayer.toJS(), customizer);
-      return Map(mergedValues);
-    });
+    const mergedlistOfProductsLayer = listOfProductsLayer.reduce(
+      (acc, productLayer) => {
+        const mergedValues = _.mergeWith(
+          acc.toJS(),
+          productLayer.toJS(),
+          customizer
+        );
+        return Map(mergedValues);
+      }
+    );
 
-    const normalizeFinalLayer = mergedlistOfProductsLayer
-                                .mapKeys(key => _.replace(key, 'prod', 'imp'));
+    const normalizeFinalLayer = mergedlistOfProductsLayer.mapKeys(key =>
+      _.replace(key, 'prod', 'imp')
+    );
 
     return normalizeFinalLayer;
   }
@@ -240,7 +296,8 @@ const buildActiveFilters = (filtersData = {}) => {
     sellingAids = Map({}),
     filterGroup = List(),
     productsNumber = 0,
-    activeFilters = Map({}) } = filtersData;
+    activeFilters = Map({}),
+  } = filtersData;
 
   const aid = activeFilters.get('aid');
   const filters = activeFilters.get('filters');
@@ -264,22 +321,25 @@ const buildActiveFilters = (filtersData = {}) => {
     });
   }
 
-  const dataLayer = buildFiltersDataLayer([aidFilters, appliedFilters, availabilityFilters]);
+  const dataLayer = buildFiltersDataLayer([
+    aidFilters,
+    appliedFilters,
+    availabilityFilters
+  ]);
   const dataLayerWithResult = addResultToLayer(productsNumber, dataLayer);
   return dataLayerWithResult;
 };
 
 const clearFilters = (dataLayer = Map({})) =>
   dataLayer
-    .set('filter_value', List())
-    .set('filter_name', List())
-    .set('filter_result', '');
+    .set(LABEL.FILTER_VALUE, List())
+    .set(LABEL.FILTER_NAME, List())
+    .set(LABEL.FILTER_RESULT, '');
 
 export {
   buildPageName,
   buildProductLayer,
   buildRelatedProductsLayer,
   buildActiveFilters,
-  clearFilters
+  clearFilters,
 };
-
