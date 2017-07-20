4a29d723618b7740edeb3f32c1fd006e0065774d
Rinaldo <rrossi@byte-code.com>
2017-07-20 16:36:52 +0200

    add --log-analytics command line option

--- app/CommandLineOptions.js
+++ app/CommandLineOptions.js
@@ -2,3 +2,4 @@ const argv = require('electron').remote.process.argv;
 
 export const isDebugMode = argv.indexOf('--debug') > 0;
 export const isDevToolMode = argv.indexOf('--dev-tool') > 0;
+export const isAnalyticsLogMode = argv.indexOf('--log-analytics') > 0;
