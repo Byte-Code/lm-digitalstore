aa6b6a4f21b9a8edcc05107b19cfd4f7d8b47686
Alessandro Palumbo <apalumbo@byte-code.com>
2018-03-15 11:44:29 +0100

    Screen saver now reads the video from world instead of have it hardcoded

--- app/components/ScreenSaver.js
+++ app/components/ScreenSaver.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import video from '../../app/assets/videos/DS_Splash02.mp4';
 import styles from './styles.css';
+import PropTypes from 'prop-types';
 
 export default class ScreenSaver extends Component {
+  static propTypes = {
+    video: PropTypes.any ,
+  };
+
   render() {
+    const { video } = this.props;
+
     return (
       <video
         autoPlay
         loop
         muted
         className={styles.video}
+
       >
         <source src={video} type="video/mp4" />
       </video>
