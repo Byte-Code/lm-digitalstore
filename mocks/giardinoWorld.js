8574722d6aae2fe0d7a7d0fe52b31877b2ce5537
Alessandro Palumbo <apalumbo@byte-code.com>
2018-03-15 11:42:30 +0100

    Added worlds on different files

---
+++ mocks/giardinoWorld.js
@@ -0,0 +1,85 @@
+import footer from '../app/assets/giardinoImg/00_Footer.png';
+import tavoliSedie from '../app/assets/giardinoImg/01_TavoliSedie.png';
+import carport from '../app/assets/giardinoImg/02_Carport.jpg';
+import panchine from '../app/assets/giardinoImg/03_Panchine.jpg';
+import sedie from '../app/assets/giardinoImg/04_SediePoltrone.jpg';
+import tavoli from '../app/assets/giardinoImg/05_Tavoli.jpg';
+import dondoli from '../app/assets/giardinoImg/06_DondoliAmache.jpg';
+import pergole from '../app/assets/giardinoImg/07_Pergole.jpg';
+import divisori from '../app/assets/giardinoImg/08_DivisoriGiardino.png';
+import ombrelloni from '../app/assets/giardinoImg/09_OmbrelloniBasi.jpg';
+import gazebo from '../app/assets/giardinoImg/10_Gazebo.jpg';
+import sdraio from '../app/assets/giardinoImg/11_SdraioLettini.jpg';
+import texture from '../app/assets/giardinoImg/texture.png';
+import video from '../app/assets/videos/video-splash.mp4';
+
+const world = {
+  worldName: 'Giardino e Terrazzo',
+  worldTemplateName : 'Giardino',
+  screenSaverVideo : video,
+  title: 'Scopri le soluzioni per Giardino e Terrazzo',
+  trailingImage: footer,
+  banner: {
+    text: 'Scopri le funzioni del Digital Store',
+    image: texture
+  },
+  families: [
+    {
+      familyName: 'Tavoli',
+      image: tavoli,
+      categoryCode: 'CAT656'
+    },
+    {
+      familyName: 'Gazebo',
+      image: gazebo,
+      categoryCode: 'CAT944'
+    },
+    {
+      familyName: 'Sedie e Poltrone',
+      image: sedie,
+      categoryCode: 'CAT657'
+    },
+    {
+      familyName: 'Panchine',
+      image: panchine,
+      categoryCode: 'CAT945'
+    },
+    {
+      familyName: 'Dondoli e Amache',
+      image: dondoli,
+      categoryCode: 'CAT659'
+    },
+    {
+      familyName: 'Ombrelloni e Basi',
+      image: ombrelloni,
+      categoryCode: 'CAT660'
+    },
+    {
+      familyName: 'Set di tavoli e sedie',
+      image: tavoliSedie,
+      categoryCode: 'CAT655'
+    },
+    {
+      familyName: 'Sdraio e Lettini',
+      image: sdraio,
+      categoryCode: 'CAT658'
+    },
+    {
+      familyName: 'Pergole',
+      image: pergole,
+      categoryCode: 'CAT676'
+    },
+    {
+      familyName: 'Carport',
+      image: carport,
+      categoryCode: 'CAT4231'
+    },
+    {
+      familyName: 'Divisori Giardino',
+      image: divisori,
+      categoryCode: 'CAT4097'
+    }
+  ]
+};
+
+export default world;
