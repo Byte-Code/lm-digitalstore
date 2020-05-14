b961e6aab4bedf78fd2382b95a7488682f4114bc
Alessandro Palumbo <apalumbo@byte-code.com>
2020-05-14 17:23:47 +0200

    Nuovo giardino 2020

--- mocks/world.js
+++ mocks/world.js
@@ -1,11 +1,13 @@
 import giardino from './giardinoWorld';
 import giardino2019 from './giardinoWorld2019';
+import giardino2020 from './giardinoWorld2020';
 import comfort from './comfortWorld';
 
 const availableWorlds = {
   comfort,
   giardino,
-  giardino2019
+  giardino2019,
+  giardino2020
 };
 
-export default availableWorlds.comfort;
+export default availableWorlds.giardino2020;
