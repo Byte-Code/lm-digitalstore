fcb9d973f9c90461940aed85d7b5d7c1afbeb25b
aleturolla <aleturolla.23@gmail.com>
2021-02-24 16:54:37 +0100

    Giardino 2021

--- app/utils/store-code-utils.js
+++ app/utils/store-code-utils.js
@@ -3,12 +3,12 @@ const newNetworkWhitelist = [132];
 const networkWhitelist = [...oldNetworkWhitelist, ...newNetworkWhitelist];
 
 export function isWhitelisted(ipAddress) {
-  const bits = ipAddress.split(".");
+  const bits = ipAddress.split('.');
   return networkWhitelist.includes(parseInt(bits[1]));
 }
 
 export function getStoreCodeFromIpAddress(ipAddress) {
-  const bits = ipAddress.split(".");
+  const bits = ipAddress.split('.');
   return String(
     oldNetworkWhitelist.includes(bits[1])
       ? Math.floor(bits[2] / 2)
